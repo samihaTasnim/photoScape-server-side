8e8d9140aadf9bacd92696769e938de374780bc5
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-04-15 23:20:47 +0600

    review and testimonail added

--- index.js
+++ index.js
@@ -6,8 +6,51 @@ app.use(express.json())
 app.use(cors())
 const port = process.env.PORT || 5000
 
+const pass = 'N@AF9_q-gBmUZdx'
+const user = 'samFirstDatabase'
+const dbname = 'myFirstDatabase'
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = `mongodb+srv://${user}:${pass}@cluster0.5rt5l.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+
+  console.log('connected');
+  const collection = client.db("travally").collection("services");
+  app.post('/addservice', (req, res) => {
+    collection.insertOne(req.body)
+    .then(result => console.log(result)) 
+    .catch(err => console.log(err))
+  })
+
+  app.get('/services', (req, res) => {
+    collection.find({})
+    .toArray((err, docs) => {
+      res.send(docs)
+    })
+  })
+  
+  const reviewcollection = client.db("travally").collection("reviews");
+  app.post('/addreview', (req, res) => {
+    reviewcollection.insertOne(req.body)
+    .then(result => console.log(result)) 
+    .catch(err => console.log(err))
+  })
+  
+  app.get('/testimonials', (req, res) => {
+    reviewcollection.find({})
+    .toArray((err, docs) => {
+      res.send(docs)
+    })
+  })
+  
+  
+});
 
 app.listen(port)
